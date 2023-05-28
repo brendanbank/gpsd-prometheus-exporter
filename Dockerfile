bef0b18023a54fe8606615af9db782e744003ad5
NMC <n@nmc.dev>
2023-05-28 00:33:33 -0400

    Add dockerfile, entrypoint and github actions.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:alpine3.10
+
+RUN pip install prometheus_client
+RUN pip install gps
+
+WORKDIR /app
+
+COPY . /app
+
+ENV GEOPOINT_LON=38.897809878104574
+ENV GEOPOINT_LAT=-77.03655125936501
+
+
+CMD ["entrypoint.sh"]
+
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+
+EXPORTER_ARGS = ""
+
+[[ -z "${GPSD_HOST}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --hostname ${GPSD_HOST}"
+[[ -z "${GPSD_PORT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --port ${GPSD_PORT}"
+[[ -z "${EXPORTER_PORT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --exporter-port ${EXPORTER_PORT}"
+
+[[ -z "${GEOPOINT_LON}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lon ${GEOPOINT_LON}"
+[[ -z "${GEOPOINT_LAT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lat ${GEOPOINT_LAT}"
+
+./gpsd_exporter.py -v --pps-histogram --offset-from-geopoint $EXPORTER_ARGS
+
