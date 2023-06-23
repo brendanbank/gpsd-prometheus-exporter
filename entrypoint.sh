5e9adf0db7c052a5fb91441062101db3ae1669c2
NMC <n@nmc.dev>
2023-06-22 23:06:59 -0400

    Add debug and port config.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 EXPORTER_ARGS=""
 
+[[ -z "${DEBUG}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} -d"
+
 [[ -z "${GPSD_HOST}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --hostname ${GPSD_HOST}"
 [[ -z "${GPSD_PORT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --port ${GPSD_PORT}"
 [[ -z "${EXPORTER_PORT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --exporter-port ${EXPORTER_PORT}"
@@ -9,5 +11,6 @@ EXPORTER_ARGS=""
 [[ -z "${GEOPOINT_LON}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lon ${GEOPOINT_LON}"
 [[ -z "${GEOPOINT_LAT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lat ${GEOPOINT_LAT}"
 
+echo ./gpsd_exporter.py -v --pps-histogram --offset-from-geopoint $EXPORTER_ARGS
 ./gpsd_exporter.py -v --pps-histogram --offset-from-geopoint $EXPORTER_ARGS
 
