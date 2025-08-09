f26523144abf6b44e55f91ac91296426335246eb
Brendan Bank <brendan@BGW-BB003.bgwlan.nl>
2025-08-09 07:28:06 +0200

    Refactor Dockerfile to use Debian slim image and install Python dependencies via apt. Update entrypoint.sh to remove bash dependency and improve argument handling with clearer conditional checks.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,51 @@
 
 EXPORTER_ARGS=""
 
-[[ "${DEBUG}" != "0" && -n "${DEBUG}" ]] && EXPORTER_ARGS="${EXPORTER_ARGS} -d"
+if [ "${DEBUG}" != "0" ] && [ -n "${DEBUG}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} -d"
+fi
 
 # Add verbose flag if VERBOSE is set
-[[ -n "${VERBOSE}" ]] && EXPORTER_ARGS="${EXPORTER_ARGS} -v"
+if [ -n "${VERBOSE}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} -v"
+fi
 
-[[ -z "${GPSD_HOST}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --hostname ${GPSD_HOST}"
-[[ -z "${GPSD_PORT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --port ${GPSD_PORT}"
-[[ -z "${EXPORTER_PORT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --exporter-port ${EXPORTER_PORT}"
+if [ -n "${GPSD_HOST}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --hostname ${GPSD_HOST}"
+fi
+if [ -n "${GPSD_PORT}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --port ${GPSD_PORT}"
+fi
+if [ -n "${EXPORTER_PORT}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --exporter-port ${EXPORTER_PORT}"
+fi
 
-[[ -z "${GEOPOINT_LON}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lon ${GEOPOINT_LON}"
-[[ -z "${GEOPOINT_LAT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lat ${GEOPOINT_LAT}"
+if [ -n "${GEOPOINT_LON}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lon ${GEOPOINT_LON}"
+fi
+if [ -n "${GEOPOINT_LAT}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --geopoint-lat ${GEOPOINT_LAT}"
+fi
 
-[[ -z "${GEO_BUCKET_SIZE}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geo-bucket-size ${GEO_BUCKET_SIZE}"
-[[ -z "${GEO_BUCKET_COUNT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --geo-bucket-count ${GEO_BUCKET_COUNT}"
+if [ -n "${GEO_BUCKET_SIZE}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --geo-bucket-size ${GEO_BUCKET_SIZE}"
+fi
+if [ -n "${GEO_BUCKET_COUNT}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --geo-bucket-count ${GEO_BUCKET_COUNT}"
+fi
 
-[[ -z "${PPS_BUCKET_SIZE}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --pps-bucket-size ${PPS_BUCKET_SIZE}"
-[[ -z "${PPS_BUCKET_COUNT}" ]] || EXPORTER_ARGS="${EXPORTER_ARGS} --pps-bucket-count ${PPS_BUCKET_COUNT}"
+if [ -n "${PPS_BUCKET_SIZE}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --pps-bucket-size ${PPS_BUCKET_SIZE}"
+fi
+if [ -n "${PPS_BUCKET_COUNT}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --pps-bucket-count ${PPS_BUCKET_COUNT}"
+fi
 
 # Add PPS histogram support if PPS_TIME1 is set
-[[ -n "${PPS_TIME1}" ]] && EXPORTER_ARGS="${EXPORTER_ARGS} --pps-histogram --pps-time1 ${PPS_TIME1}"
+if [ -n "${PPS_TIME1}" ]; then
+  EXPORTER_ARGS="${EXPORTER_ARGS} --pps-histogram --pps-time1 ${PPS_TIME1}"
+fi
 
-echo ./gpsd_exporter.py --offset-from-geopoint $EXPORTER_ARGS
-./gpsd_exporter.py --offset-from-geopoint $EXPORTER_ARGS
+echo ./gpsd_exporter.py --offset-from-geopoint ${EXPORTER_ARGS}
+./gpsd_exporter.py --offset-from-geopoint ${EXPORTER_ARGS}
 
